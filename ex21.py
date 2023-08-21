bc8a8463428dfec4d4c237ac51691413e4c7c41f
Okayama-IoT-AI-Security-Startup <120706225+Okayama-IoT-AI-Security-Startup@users.noreply.github.com>
2023-08-21 10:04:11 +0900

    Update ex21.py
    cv2の動作確認のための2つのファイルについてコメントを記入した

--- ex21.py
+++ ex21.py
@@ -1,3 +1,10 @@
+# open-cvがインストールされているかは，import vc2 でチェックしてみてください．
+# python -c "import cv2"
+# で，エラーが戻らないことを確認します．
+# "lena.jpg" は https://github.com/opencv/opencv/raw/4.x/samples/data/lena.jpg
+#  "haarcascade_frontalface_alt.xml" は https://github.com/opencv/opencv/raw/4.x/data/haarcascades/haarcascade_frontalface_alt.xml
+# もしくは，cv2.data.haacascades に記述されています（python -c "import cv2; print(cv2.data.haarcascades)"）
+
 import cv2
 img = cv2.imread("lena.jpg")
 gimg=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
