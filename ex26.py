ba796db313e2958617c22b4e2a8754ca1174abb2
Okayama-IoT-AI-Security-Startup <120706225+Okayama-IoT-AI-Security-Startup@users.noreply.github.com>
2023-08-28 16:51:09 +0900

    Update ex26.py
    寝ぼけたコードを削除

--- ex26.py
+++ ex26.py
@@ -19,7 +19,7 @@ with mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence
     if results.multi_face_landmarks:
       for face_landmarks in results.multi_face_landmarks:
          wks=np.array([[int(face_landmarks.landmark[i].x*w), int(face_landmarks.landmark[i].y*h)] for i in masks])
-         cv2.fillConvexPoly(image, wks, color=(200, 200, 20np.array0))
+         cv2.fillConvexPoly(image, wks, color=(200, 200, 200))
     cv2.imshow('MediaPipe FaceMesh', image)
     if cv2.waitKey(5) & 0xFF == ord('q'):
       break
